b7c2017a4bf2acaf073373f185eeeada844e755c
Luis Marquez <luisjose0527@gmail.com>
2023-01-19 18:22:49 -0400

    GET y parametros de ruta

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.title = 'Learning FastAPI'
 app.version = '0.0.1'
 
+movies = [
+    {
+        'id':1,
+        'tittle': 'Spiderman',
+        'overview': 'Peter Parker fue mordido por una arana y ahora tiene superpoderes',
+        'year': 2017,
+        'rating': 7.2,
+        'category': 'accion'
+    },
+    {
+        'id':2,
+        'tittle': 'El gato con botas',
+        'overview': 'Antonio Banderas hace la voz de un gato muy mono',
+        'year': 2022,
+        'rating': 7.9,
+        'category': 'animacion'
+    }
+]
+
 @app.get('/', tags=['home'])
 def message():
-    return "Holis como andas, esto lo hice desde python"
+    return HTMLResponse('<h1>HOLA</h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
+
+@app.get('/movies/{id}', tags=['movies'])
+def get_movie(id:int):
+    for item in movies:
+        if item['id'] == id:
+            return item
+    return []
