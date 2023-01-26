74cf916bd9967d362eb417617cf5cf2efb89ec66
Luis Marquez <luisjose0527@gmail.com>
2023-01-26 10:53:55 -0400

    Creando servicios

--- routers/movie.py
+++ routers/movie.py
@@ -6,6 +6,7 @@ from config.database import Session
 from models.movie import Movie as MovieModel
 from fastapi.encoders import jsonable_encoder
 from middlewares.jwt_bearer import JWTBearer
+from services.movie import MovieService
 
 movie_router = APIRouter()
 
@@ -32,13 +33,13 @@ class Movie(BaseModel):
 @movie_router.get('/movies', tags=['movies'], response_model=List[Movie])
 def get_movies() -> List[Movie]:
     db = Session()
-    result = db.query(MovieModel).all()
+    result = MovieService(db).get_movies()
     return JSONResponse(content=jsonable_encoder(result))
 
 @movie_router.get('/movies/{id}', tags=['movies'], response_model=Movie)
 def get_movie(id:int = Path(ge=1)) -> Movie:
     db = Session()
-    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    result = MovieService(db).get_movie(id)
     if not result:
         return JSONResponse(status_code=404, content={'message':'No encontrado'})
     else:
@@ -47,7 +48,7 @@ def get_movie(id:int = Path(ge=1)) -> Movie:
 @movie_router.get('/movies/', tags=['movies'], response_model=List[Movie])
 def get_movie_by_category(category:str = Query(min_length=5, max_length=20)) -> List[Movie]:
     db = Session()
-    result = db.query(MovieModel).filter(MovieModel.category == category).all()
+    result = MovieService(db).get_movie_by_category(category)
     if not result:
         return JSONResponse(status_code=200, content={'message':'No hay peliculas de esa categoria'})
     else:
@@ -64,7 +65,7 @@ def create_movie(movie:Movie) -> dict:
 @movie_router.put("/movies/{id}", tags=["movies"], response_model=dict, dependencies=[Depends(JWTBearer())])
 def update_movie(id:int, movie:Movie) -> dict:
     db=Session()
-    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    result = MovieService(db).get_movie(id)
     if result:
         result.tittle = movie.tittle
         result.overview = movie.overview
@@ -79,7 +80,7 @@ def update_movie(id:int, movie:Movie) -> dict:
 @movie_router.delete("/movies/{id}", tags=["movies"], response_model=dict, dependencies=[Depends(JWTBearer())])
 def delete_movie(id:int) -> dict:
     db=Session()
-    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    result = MovieService(db).get_movie(id)
     if result:
         db.delete(result)
         db.commit()
